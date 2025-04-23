9f70fba3d2f5797e3e5e5326e43c8935b4d6d795
Dink873 <diana.dovbeta4102@gmail.com>
2025-04-24 01:36:46 +0300

    3 laba

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
 import 'package:my_project/main.dart';
+import 'package:my_project/screens/login_screen.dart';
+import 'package:my_project/screens/main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  testWidgets('Перевірка генерації випадкового числа', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Navigation based on login status', (WidgetTester tester) async {
+    // Ініціалізуємо SharedPreferences перед тестом
+    final prefs = await SharedPreferences.getInstance();
 
-    expect(find.byType(Text), findsWidgets);
+    // Установлюємо значення для isLoggedIn в SharedPreferences (false для неавторизованого користувача)
+    await prefs.setBool('isLoggedIn', false);
 
-    final button = find.text("Згенерувати");
-    expect(button, findsOneWidget);
+    // Створюємо віджет з переданим значенням isLoggedIn
+    await tester.pumpWidget(MyApp(isLoggedIn: false));
 
-    await tester.tap(button);
-    await tester.pump();
+    // Перевіряємо, що сторінка логіну відображається
+    expect(find.byType(LoginScreen), findsOneWidget);
+    expect(find.byType(MainScreen), findsNothing);
 
-    expect(find.byType(Text), findsWidgets);
+    // Тепер встановлюємо isLoggedIn як true (для авторизованого користувача)
+    await prefs.setBool('isLoggedIn', true);
+
+    // Створюємо віджет з переданим значенням isLoggedIn
+    await tester.pumpWidget(MyApp(isLoggedIn: true));
+
+    // Перевіряємо, що головна сторінка відображається
+    expect(find.byType(MainScreen), findsOneWidget);
+    expect(find.byType(LoginScreen), findsNothing);
   });
 }
